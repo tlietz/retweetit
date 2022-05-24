37a7493dbce98c71c3c2c499706893832e8e27db
tlietz <theodore.lietz@gmail.com>
2022-05-24 11:50:14 -0500

    initial script

---
+++ main.py
@@ -0,0 +1,13 @@
+import tweepy
+
+from constants import *
+
+auth = tweepy.OAuth1UserHandler(
+    consumer_key, consumer_secret, access_token, access_token_secret
+)
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print(tweet.text)
