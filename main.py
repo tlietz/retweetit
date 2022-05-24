6bf2d69a9e0fdd58099baf9c2f3d785e8bb2af41
tlietz <theodore.lietz@gmail.com>
2022-05-24 13:07:27 -0500

    get first tweet in news feed

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ auth = tweepy.OAuth1UserHandler(
     API_KEY, API_KEY_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
 )
 
-api = tweepy.API(auth)
-
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
+client = tweepy.Client(bearer_token=BEARER_TOKEN, consumer_key=API_KEY, consumer_secret=API_KEY_SECRET,
+                       access_token=ACCESS_TOKEN, access_token_secret=ACCESS_TOKEN_SECRET)
+query = 'news'
+tweets = client.search_recent_tweets(query=query, max_results=10)
+print(tweets.data[0])
